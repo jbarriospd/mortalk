88b05cfb34346272e60068fcfa062754041804c2
josebarrios <jose.barrios@servinformacion.com>
2019-09-25 17:14:23 -0500

    add fatality

---
+++ app.js
@@ -0,0 +1,58 @@
+function showModal() {
+    document.getElementById('openModal').style.display = 'block';
+}
+
+function showModalKano() {
+    document.getElementById('openModalKano').style.display = 'block';
+}
+
+function showModalLiu() {
+    document.getElementById('openModalLiuKang').style.display = 'block';
+}
+
+function showModalRaiden() {
+    document.getElementById('openModalRaiden').style.display = 'block';
+}
+
+function showModalScorpion() {
+    document.getElementById('openModalScorpion').style.display = 'block';
+}
+
+function showModalSonya() {
+    document.getElementById('openModalSonya').style.display = 'block';
+}
+
+function showModalSubZero() {
+    document.getElementById('openModalSub-zero').style.display = 'block';
+}
+
+function showModalReptile() {
+    document.getElementById('openModalReptile-zero').style.display = 'block';
+}
+
+function showModalGoro() {
+    document.getElementById('openModalGoro').style.display = 'block';
+}
+
+function showModalShang() {
+    document.getElementById('openModalShang').style.display = 'block';
+}
+
+
+
+
+
+function CloseModal() {
+    document.getElementById('openModal').style.display = 'none';
+    document.getElementById('openModalKano').style.display = 'none';
+    document.getElementById('openModalLiuKang').style.display = 'none';
+    document.getElementById('openModalRaiden').style.display = 'none';
+    document.getElementById('openModalScorpion').style.display = 'none';
+    document.getElementById('openModalSonya').style.display = 'none';
+    document.getElementById('openModalSub-zero').style.display = 'none';
+    document.getElementById('openModalReptile').style.display = 'none';
+    document.getElementById('openModalGoro').style.display = 'none';
+    document.getElementById('openModalShang').style.display = 'none';
+
+
+}
